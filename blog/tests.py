5155f59daac8c38eec1d11097f0dbd475affbb49
Ravis <aao.t@yandex.ru>
2018-10-13 21:57:29 +0300

    tests

--- blog/tests.py
+++ blog/tests.py
@@ -7,13 +7,22 @@ class CRUDTest(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def test_show_index(self):
+        assert self.client.get('/blog/').status_code == 200
+
+    def test_delete(self):
+        Post.objects.create(title='title1', text='text1', image='/image.jpg')
+        self.client.get('/blog/delete/1/')
+        assert Post.objects.count() == 0
+
     def test_add_post(self):
         response = self.client.post(
-            'blog/',
+            '/blog/create/',
             data={
-                "title":"title_test",
+                "title": "title_test",
                 "text": "text ad;fkasd;fkj",
                 "image": "/home/image.jpg"
             }
         )
-        assert Post.objects.first()
+        assert response.status_code == 302
+        assert Post.objects.all().exists()
